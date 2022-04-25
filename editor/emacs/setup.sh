a05d3e6bae897d207beed35ae9b79b475f38eab7
rafiyq <rafiyq@protonmail.com>
2022-04-26 04:42:07 +0700

    move script to bin directory

--- editor/emacs/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-spacemacs_url="https://github.com/syl20bnr/spacemacs"
-spacemacs_path="$HOME/.config/spacemacs"
-
-# clone spacemacs
-[ ! -d $spacemacs_path/master ] && git clone -b master $spacemacs_url $spacemacs_path/master
-[ ! -d $spacemacs_path/develop ] && git clone -b develop $spacemacs_url $spacemacs_path/develop
