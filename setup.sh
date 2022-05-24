9146ffeb55da97183f6cbdb64036ce61525fd10f
rafiyq <rafiyq@protonmail.com>
2022-05-24 10:02:13 +0700

    clean up setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -sfvi $PWD/editor/nvim $HOME/.config/
 ln -sfvi $PWD/editor/helix $HOME/.config/
 #ln -sfvi $PWD/editor/vim/vimrc $HOME/.vimrc
 ln -sfvi $PWD/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
+ln -sfvi $PWD/zsh $HOME/.config/
 
 # Applications
 # ln -sfv $PWD/apps/firefox-private.desktop $HOME/.local/share/applications/
@@ -15,10 +16,3 @@ ln -sfvi $PWD/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
 #ln -sfv $PWD/apps/emacs-term.desktop $HOME/.local/share/applications/
 #ln -sfv $PWD/apps/spacemacs.desktop $HOME/.local/share/applications/
 #ln -sfv $PWD/apps/spacemacs-term.desktop $HOME/.local/share/applications/
-
-# zsh
-ln -sfvi $PWD/zsh $HOME/.config/
-
-#$PWD/zsh/grml-zsh.sh
-#$PWD/zsh/oh-my-zsh.sh
-#$PWD/editor/emacs/setup.sh
