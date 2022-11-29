304cb176f65b8610e5dee5f487a86335c9474e96
rafiyq <rafiyq@protonmail.com>
2022-11-29 07:32:00 +0000

    git

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ ln -sfvi $PWD/editor/helix $HOME/.config/
 #ln -sfvi $PWD/editor/vim/vimrc $HOME/.vimrc
 ln -sfvi $PWD/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
 ln -sfvi $PWD/shell/zsh $HOME/.config/
+ln -sfvi $PWD/shell/fish $HOME/.config/
 
 # Applications
 # ln -sfv $PWD/apps/firefox-private.desktop $HOME/.local/share/applications/
