5e50e0e891c83ca96f2bf0e0c272a0c50a4d11d0
rafiyq <rafiyq@protonmail.com>
2022-12-01 21:25:59 +0700

    fix README

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # config
-ln -sfvi $PWD/bin $HOME/.local/
 ln -sfvi $PWD/tmux/tmux.conf $HOME/.tmux.conf
 ln -sfvi $PWD/git $HOME/.config/
 ln -sfvi $PWD/editor/nvim $HOME/.config/
@@ -10,6 +9,9 @@ ln -sfvi $PWD/editor/helix $HOME/.config/
 ln -sfvi $PWD/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
 ln -sfvi $PWD/shell/zsh $HOME/.config/
 ln -sfvi $PWD/shell/fish $HOME/.config/
+
+# Scripts
+#ln -sfvi $PWD/bin $HOME/.local/
 
 # Applications
 # ln -sfv $PWD/apps/firefox-private.desktop $HOME/.local/share/applications/
