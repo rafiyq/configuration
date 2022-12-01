3be2e5c5bf9e11a774ce3a8fb98dc97c22068da7
rafiyq <rafiyq@protonmail.com>
2022-12-01 22:14:29 +0700

    detect configuration location

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
+HERE="$( cd "$(dirname "$0")" ; pwd -P )"
 
 # config
-ln -sfvi $PWD/tmux/tmux.conf $HOME/.tmux.conf
-ln -sfvi $PWD/git $HOME/.config/
-ln -sfvi $PWD/editor/nvim $HOME/.config/
-ln -sfvi $PWD/editor/helix $HOME/.config/
-#ln -sfvi $PWD/editor/vim/vimrc $HOME/.vimrc
-ln -sfvi $PWD/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
-ln -sfvi $PWD/shell/zsh $HOME/.config/
-ln -sfvi $PWD/shell/fish $HOME/.config/
+ln -sfvi $HERE/tmux/tmux.conf $HOME/.tmux.conf
+ln -sfvi $HERE/git $HOME/.config/
+ln -sfvi $HERE/editor/nvim $HOME/.config/
+ln -sfvi $HERE/editor/helix $HOME/.config/
+#ln -sfvi $HERE/editor/vim/vimrc $HOME/.vimrc
+ln -sfvi $HERE/editor/sublime-text/* $HOME/.config/sublime-text/Packages/User/
+ln -sfvi $HERE/shell/zsh $HOME/.config/
+ln -sfvi $HERE/shell/fish $HOME/.config/
 
 # Scripts
-#ln -sfvi $PWD/bin $HOME/.local/
+ln -sfvi $HERE/script/sh $HOME/.local/bin/
+ln -sfvi $HERE/script/python $HOME/.local/bin/
+ln -sfvi $HERE/script/nautilus $HOME/.local/bin/
 
 # Applications
-# ln -sfv $PWD/apps/firefox-private.desktop $HOME/.local/share/applications/
-#ln -sfv $PWD/apps/emacs.desktop $HOME/.local/share/applications/
-#ln -sfv $PWD/apps/emacs-term.desktop $HOME/.local/share/applications/
-#ln -sfv $PWD/apps/spacemacs.desktop $HOME/.local/share/applications/
-#ln -sfv $PWD/apps/spacemacs-term.desktop $HOME/.local/share/applications/
+# ln -sfv $HERE/apps/firefox-private.desktop $HOME/.local/share/applications/
+#ln -sfv $HERE/apps/emacs.desktop $HOME/.local/share/applications/
+#ln -sfv $HERE/apps/emacs-term.desktop $HOME/.local/share/applications/
+#ln -sfv $HERE/apps/spacemacs.desktop $HOME/.local/share/applications/
+#ln -sfv $HERE/apps/spacemacs-term.desktop $HOME/.local/share/applications/
