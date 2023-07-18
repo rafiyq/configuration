93175f65a06fcb6bce28f2812210d6795021e74a
Rafiyq <rafiyq@outlook.com>
2023-07-18 22:50:32 +0700

    Add Android SDK to path

--- shell/fish/conf.d/environment.fish
+++ shell/fish/conf.d/environment.fish
@@ -2,3 +2,8 @@
 set VERSION v18.16.1
 set DISTRO linux-x64
 fish_add_path /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin
+
+# Android SDK
+set ANDROID_HOME $HOME/Android/Sdk
+fish_add_path --append $ANDROID_HOME/emulator
+fish_add_path --append $ANDROID_HOME/platform-tools
