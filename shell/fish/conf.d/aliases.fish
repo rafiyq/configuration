755f1d9e9b969832c417e62d64ea527fa2f7f1ac
Rafiyq <rafiyq@outlook.com>
2023-07-18 20:30:30 +0700

    Split alias to config.d

---
+++ shell/fish/conf.d/aliases.fish
@@ -0,0 +1,16 @@
+alias clean-swap='sudo swapoff -a && sudo swapon -a'
+alias dl='aria2c --split=16 --continue=true --max-connection-per-server=16 --max-upload-limit=5K --seed-time=0'
+
+# Git shortcut
+alias gss='git status --short'
+alias gst='git status'
+
+alias ga='git add'
+alias gaa='git add --all'
+
+alias gcmsg='git commit --message'
+alias gcam='git commit --all --message'
+
+alias gp='git push'
+alias gd='git diff'
+alias gc='git clone --recurse-submodules'
--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -3,20 +3,6 @@ if status is-interactive
     set -U fish_greeting
 end
 
-# Git shortcut
-alias gss='git status --short'
-alias gst='git status'
-
-alias ga='git add'
-alias gaa='git add --all'
-
-alias gcmsg='git commit --message'
-alias gcam='git commit --all --message'
-
-alias gp='git push'
-alias gd='git diff'
-alias gc='git clone --recurse-submodules'
-
 # Environment variable
 set -gx EDITOR nvim
 set -gx BROWSER none
