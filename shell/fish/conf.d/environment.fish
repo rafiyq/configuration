97cc4f5454581d1eeb1907daef0954060a474711
Rafiyq <rafiyq@outlook.com>
2023-07-20 09:35:06 +0700

    Set Java and Android SDK environment

--- shell/fish/conf.d/environment.fish
+++ shell/fish/conf.d/environment.fish
@@ -4,6 +4,9 @@ set DISTRO linux-x64
 fish_add_path /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin
 
 # Android SDK
-set ANDROID_HOME $HOME/Android/Sdk
+set -x ANDROID_HOME $HOME/Android/Sdk
 fish_add_path --append $ANDROID_HOME/emulator
 fish_add_path --append $ANDROID_HOME/platform-tools
+
+# Java
+set -x JAVA_HOME $HOME/.jdks/jdk-20.0.1+9
