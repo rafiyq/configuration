c682ccf44be967f4cc334a3265081e2cc5304bcc
Rafiyq <rafiyq@outlook.com>
2023-10-13 12:52:06 +0700

    Change environment

--- shell/fish/conf.d/environment.fish
+++ shell/fish/conf.d/environment.fish
@@ -1,5 +1,5 @@
 # Nodejs
-set VERSION v18.16.1
+set VERSION v18.17.1
 set DISTRO linux-x64
 fish_add_path /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin
 
@@ -9,4 +9,15 @@ fish_add_path --append $ANDROID_HOME/emulator
 fish_add_path --append $ANDROID_HOME/platform-tools
 
 # Java
-set -x JAVA_HOME $HOME/.jdks/jdk-20.0.1+9
+# corretto-19.0.2
+set -x JAVA_HOME $HOME/.jdks/corretto-19.0.2
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+function conda-init
+    if test -f /home/rafiyq/.miniconda3/bin/conda
+        eval /home/rafiyq/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    end
+end
+# <<< conda initialize <<<
+
