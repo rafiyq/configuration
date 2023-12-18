387e5b4ce192967c1a2331e878020a25a28e28e1
Rafiyq <rafiyq@outlook.com>
2023-12-18 12:44:40 +0700

    Add mamba to environment

--- shell/fish/conf.d/environment.fish
+++ shell/fish/conf.d/environment.fish
@@ -21,3 +21,11 @@ function conda-init
 end
 # <<< conda initialize <<<
 
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+function mamba-init
+    set -gx MAMBA_EXE "/opt/micromamba/bin/micromamba"
+    set -gx MAMBA_ROOT_PREFIX "$HOME/.micromamba"
+    $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+end
+# <<< mamba initialize <<<
