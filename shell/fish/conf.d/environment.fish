918614a400872c3232fcf0a89aa3e2077a681261
Rafiyq <rafiyq@outlook.com>
2024-05-01 22:04:46 +0700

    Add rustup to path

--- shell/fish/conf.d/environment.fish
+++ shell/fish/conf.d/environment.fish
@@ -12,11 +12,14 @@ fish_add_path --append $ANDROID_HOME/platform-tools
 # corretto-19.0.2
 set -x JAVA_HOME $HOME/.jdks/corretto-19.0.2
 
+# Rust
+source $HOME/.cargo/env.fish
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 function conda-init
     if test -f /home/rafiyq/.miniconda3/bin/conda
-        eval /home/rafiyq/.miniconda3/bin/conda "shell.fish" "hook" $argv | source
+        eval /home/rafiyq/.miniconda3/bin/conda "shell.fish" hook $argv | source
     end
 end
 # <<< conda initialize <<<
@@ -24,7 +27,7 @@ end
 # >>> mamba initialize >>>
 # !! Contents within this block are managed by 'mamba init' !!
 function mamba-init
-    set -gx MAMBA_EXE "/opt/micromamba/bin/micromamba"
+    set -gx MAMBA_EXE /opt/micromamba/bin/micromamba
     set -gx MAMBA_ROOT_PREFIX "$HOME/.micromamba"
     $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 end
