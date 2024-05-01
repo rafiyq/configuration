1354b2c3e9ab86bd55cc0a797db83c750fb69147
Rafiyq <rafiyq@outlook.com>
2024-05-01 22:11:18 +0700

    Move function to separate file

--- shell/fish/functions/mamba-init.fish
+++
@@ -1,8 +0,0 @@
-function mamba-init
-    # >>> mamba initialize >>>
-    # !! Contents within this block are managed by 'mamba init' !!
-    set -gx MAMBA_EXE "/home/rafiyq/.local/bin/micromamba"
-    set -gx MAMBA_ROOT_PREFIX "/home/rafiyq/Development/micromamba"
-    $MAMBA_EXE shell hook --shell fish --prefix $MAMBA_ROOT_PREFIX | source
-    # <<< mamba initialize <<<
-end
