26ca96df914d797786e9b5db26b05a567d4e4744
rafiyq <rafiyq@outlook.com>
2024-07-30 13:15:29 +0000

    Add user configuration

--- nix/nixos-usb/flake.nix
+++ nix/nixos-usb/flake.nix
@@ -1,13 +1,33 @@
 {
   description = "nix build .#nixosConfigurations.nixos.config.system.build.sdImage";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ({ modulesPath, ... }: {
           imports = [ (modulesPath + "/installer/sd-card/sd-image-x86_64.nix") ];
         })
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.user = import [
+            ({ config, pkgs, ... }:{
+              home.username = "user";
+              home.homeDirectory = "/home/user";
+              home.stateVersion = "24.05";
+            })
+            ../modules/user.nix
+          ];
+        }
       ];
     };
   };
