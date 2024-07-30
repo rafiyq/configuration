13bf03c040d825d000b139d34c2e552c655d2f10
rafiyqw <rafiyqw@gmail.com>
2024-07-30 23:58:42 +0000

    Add hyprland

--- nix/nixos-usb/flake.nix
+++ nix/nixos-usb/flake.nix
@@ -8,32 +8,35 @@
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
-  outputs = { self, nixpkgs, home-manager ...} @ inputs: {
+  outputs = { self, nixpkgs, home-manager, hyprland, ...} @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        ({ inputs, pkgs, modulesPath, ... }: {
-          imports = [ (modulesPath + "/installer/sd-card/sd-image-x86_64.nix") ];
+        ({ inputs, pkgs, modulesPath, home-manager, ... }: {
+          imports = [ 
+            (modulesPath + "/installer/sd-card/sd-image-x86_64.nix")
+          ];
+
           programs.hyprland = {
             enable = true;
             package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
           };
+
+          users.users.user = {
+            name = "User";
+            isNormalUser = true;
+            shell = pkgs.fish;
+          };
+
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.user = import ../modules/user.nix;
+
+          system.stateVersion = "24.11";
         })
         ../modules/packages.nix
         home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.user = import [
-            ({ config, pkgs, ... }: {
-              home.username = "user";
-              home.homeDirectory = "/home/user";
-              home.stateVersion = "24.05";
-            })
-            ../modules/user.nix
-          ];
-        }
       ];
     };
   };
