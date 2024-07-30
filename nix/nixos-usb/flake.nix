6c76b05c0383c61678a33c0a9ce8ae4eba581a4f
rafiyq <rafiyq@outlook.com>
2024-07-30 12:57:34 +0000

    Minimal NixOS sd-image

--- nix/nixos-usb/flake.nix
+++ nix/nixos-usb/flake.nix
@@ -1,12 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  description = "nix build .#nixosConfigurations.nixos.config.system.build.sdImage";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ({ pkgs, modulesPath, ... }: {
-          imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-          environment.systemPackages = [ pkgs.neovim ];
+          imports = [ (modulesPath + "/installer/sd-card/sd-image-x86_64.nix") ];
         })
       ];
     };
