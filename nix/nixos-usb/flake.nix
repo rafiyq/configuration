e3a3bc7fd25e0c190d4be53d3b2e06bf0c190295
rafiyq <rafiyq@outlook.com>
2024-07-30 14:15:07 +0000

    Add hyprland

--- nix/nixos-usb/flake.nix
+++ nix/nixos-usb/flake.nix
@@ -5,14 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
-  outputs = { self, nixpkgs, home-manager }: {
+  outputs = { self, nixpkgs, home-manager ...} @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
       modules = [
-        ({ modulesPath, ... }: {
+        ({ inputs, pkgs, modulesPath, ... }: {
           imports = [ (modulesPath + "/installer/sd-card/sd-image-x86_64.nix") ];
+          programs.hyprland = {
+            enable = true;
+            package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+          };
         })
         ../modules/packages.nix
         home-manager.nixosModules.home-manager
@@ -20,7 +26,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.user = import [
-            ({ config, pkgs, ... }:{
+            ({ config, pkgs, ... }: {
               home.username = "user";
               home.homeDirectory = "/home/user";
               home.stateVersion = "24.05";
