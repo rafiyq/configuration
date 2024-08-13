65c32f4c37dac5ece46d891999b578c083de806b
Rafiyq <rafiyq@outlook.com>
2024-08-13 04:34:06 +0000

    Create our own NixOS on wsl flake

---
+++ nix/wsl-flake/flake.nix
@@ -0,0 +1,36 @@
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        nixos-wsl.nixosModules.default 
+        {
+          system.stateVersion = "24.05";
+          wsl.enable = true;
+          wsl.defaultUser = "user";
+        }
+
+        ({ pkgs, ... }: {
+          users.defaultUserShell = pkgs.fish;
+
+          environment.systemPackages = with pkgs; [
+            aria2
+            bat
+            ffmpeg
+            helix
+          ];
+
+          nix.settings.experimental-features = ["nix-command" "flakes"];
+        })
+
+        ../modules/packages.nix
+      ];
+    };
+  };
+}
