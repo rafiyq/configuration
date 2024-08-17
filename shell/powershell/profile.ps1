7cbb0cc4b00d700b2decf353e1a7f128ced97ab8
Rafiyq <rafiyq@outlook.com>
2024-08-17 20:35:24 +0700

    Simple powershell profile

--- shell/powershell/profile.ps1
+++ shell/powershell/profile.ps1
@@ -1,7 +1,7 @@
-# ALIASES
-# Git
+. (Join-Path -Path (Split-Path $PROFILE) -ChildPath "aliases.ps1")
+$env:Path += "$Env:USERPROFILE/bin"
 
-$PROMPT_PROFILE = "Ubuntu"
-. (-Join((Split-Path $PROFILE), "/aliases.ps1"))
-. (-Join((Split-Path $PROFILE), "/helper.ps1"))
-. (-Join((Split-Path $PROFILE), "/prompt.ps1"))
+#$PROMPT_PROFILE = "Ubuntu"
+#. (-Join((Split-Path $PROFILE), "/aliases.ps1"))
+#. (-Join((Split-Path $PROFILE), "/helper.ps1"))
+#. (-Join((Split-Path $PROFILE), "/prompt.ps1"))
