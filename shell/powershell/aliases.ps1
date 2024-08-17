035c3886be9d0b239bc6c31cc6d2be869ab7e682
Rafiyq <rafiyq@outlook.com>
2024-08-17 20:34:19 +0700

    Remove already used alias and add argument to alias

--- shell/powershell/aliases.ps1
+++ shell/powershell/aliases.ps1
@@ -4,19 +4,17 @@ function Git-StatusShort { git status --short }
 New-Alias -Name "gst" -Value Git-Status
 New-Alias -Name "gss" -Value Git-StatusShort
 
-function Git-Add { git add } 
+function Git-Add { git add $args } 
 function Git-AddAll { git add --all } 
 New-Alias -Name "ga" -Value Git-Add
 New-Alias -Name "gaa" -Value Git-AddAll
 
-function Git-CommitMessage { git commit --message } 
-function Git-CommitMessageAdd { git commit --all --message } 
+function Git-CommitMessage { git commit --message $args } 
+function Git-CommitMessageAdd { git commit --all --message $args } 
 New-Alias -Name "gcmsg" -Value Git-CommitMessage
 New-Alias -Name "gcam" -Value Git-CommitMessageAdd
 
-function Git-Push { git push } 
-function Git-Diff { git diff } 
-function Git-Clone { git clone --recurse-submodules } 
-New-Alias -Name "gp" -Value Git-Push
+function Git-Diff { git diff $args } 
+function Git-Clone { git clone --recurse-submodules $args } 
 New-Alias -Name "gd" -Value Git-Diff
-New-Alias -Name "gc" -Value Git-Clone
+New-Alias -Name "git clone" -Value Git-Clone
