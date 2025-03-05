9896142a3d15b0dd38d6d7222d814205b731e407
Rafiyq <rafiyq@outlook.com>
2025-03-05 07:58:36 +0700

    Only greetings and app alias

--- shell/powershell/profile.ps1
+++ shell/powershell/profile.ps1
@@ -1,7 +1,7 @@
-. (Join-Path -Path (Split-Path $PROFILE) -ChildPath "aliases.ps1")
-$env:Path += "$Env:USERPROFILE/bin"
+Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
 
-#$PROMPT_PROFILE = "Ubuntu"
-#. (-Join((Split-Path $PROFILE), "/aliases.ps1"))
-#. (-Join((Split-Path $PROFILE), "/helper.ps1"))
-#. (-Join((Split-Path $PROFILE), "/prompt.ps1"))
+New-Alias -Name "NVim" -Value $env:LOCALAPPDATA\nvim-0.10.4\bin\nvim.exe -Description "text editor"
+New-Alias -Name "FFmpeg" -Value $env:LOCALAPPDATA\ffmpeg-7.1\bin\ffmpeg.exe -Description "audio & video solution"
+New-Alias -Name "FFprobe" -Value $env:LOCALAPPDATA\ffmpeg-7.1\bin\ffprobe.exe -Description "audio & video solution"
+New-Alias -Name "Git" -Value $env:LOCALAPPDATA\Git-2.48.1\bin\git.exe -Description "distributed version control system"
+New-Alias -Name "Aria2" -Value $env:LOCALAPPDATA\aria2-1.37.0\aria2c.exe -Description "command-line download utility"
