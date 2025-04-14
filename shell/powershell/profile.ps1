60b159b9dc19b2bc6eda2140a47b988d11f5cf83
Rafiyq <rafiyq@outlook.com>
2025-04-14 14:31:36 +0700

    Create separate file for aliases

--- shell/powershell/profile.ps1
+++ shell/powershell/profile.ps1
@@ -1,7 +1,7 @@
 Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
 
-New-Alias -Name "NVim" -Value $env:LOCALAPPDATA\nvim-0.10.4\bin\nvim.exe -Description "text editor"
-New-Alias -Name "FFmpeg" -Value $env:LOCALAPPDATA\ffmpeg-7.1\bin\ffmpeg.exe -Description "audio & video solution"
-New-Alias -Name "FFprobe" -Value $env:LOCALAPPDATA\ffmpeg-7.1\bin\ffprobe.exe -Description "audio & video solution"
-New-Alias -Name "Git" -Value $env:LOCALAPPDATA\Git-2.48.1\bin\git.exe -Description "distributed version control system"
-New-Alias -Name "Aria2" -Value $env:LOCALAPPDATA\aria2-1.37.0\aria2c.exe -Description "command-line download utility"
+$profileDir = Split-Path -Path $PROFILE -Parent
+
+Invoke-Expression -Command (Get-Content $profileDir\aliases.ps1 -Raw)
+Invoke-Expression -Command (Get-Content $profileDir\functions.ps1 -Raw)
+
